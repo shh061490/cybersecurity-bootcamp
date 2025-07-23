9e720706672958a399dd27b230322fa9bb0bd63b
Samuel Huddleston <shh06@Hotmail.com>
2025-07-22 22:11:38 -0500

    lab1.sh

---
+++ ops1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Lab: User greeting and file check
+# TODO: Complete the script
+
+username="Hello Samuel Huddleston"
+
+if [ "$username" = "Hello Samuel Huddleston" ]; then 
+    echo $username
+else 
+
+    echo "Syslog missing!"
+
+fi
